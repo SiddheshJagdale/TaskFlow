6096633629b43c3c477bae0dccf6d0a0a105acdd
Siddhesh Jagdale <siddheshjagdale3534@gmail.com>
2025-01-10 23:41:35 +0530

    Changes .env file

--- firebaseStore/firebase.js
+++ firebaseStore/firebase.js
@@ -7,13 +7,13 @@ setLogLevel("debug");
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA40hzo5OJ7JsAXBIhtoIzyTt2FP4KVHiE",
-  authDomain: "task-management-app-8c236.firebaseapp.com",
-  projectId: "task-management-app-8c236",
-  storageBucket: "task-management-app-8c236.firebasestorage.app",
-  messagingSenderId: "743623677714",
-  appId: "1:743623677714:web:48b5a0360722ddaae2c5cb",
-  measurementId: "G-K341EPHJGL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
